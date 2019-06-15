c4ae3de8c02b15221f430305b44eb40a9ef14836
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-06-14 17:54:43 -0700

    routes working

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,17 +1,22 @@
 // could use 'db' if more than the News model was required
-const { Article } = require('../models/Article.js')
+const Article = require('../models/Article.js')
+
 
 module.exports = app => {
     // POST an article to the db
-    app.post('/articles', (req, res) => Article.insert(req.body, e => e ? console.log(e) : res.sendStatus(200)))
+    // this is working!
+    app.post('/articles', (req, res) => Article.create(req.body, e => e ? console.log(e) : res.sendStatus(200)))
 
-    // ADD a comment to an article
+    // UPDATE an article in the db
+    // this is working!
     app.put('/articles/:_id', (req, res) => Article.update({ _id: req.params._id }, { $set: req.body }, e => e ? console.log(e) : res.sendStatus(200)))
 
-    // GET all saved articles from the db
+    // GET all articles from the db
+    // this is working!
     app.get('/articles', (req, res) => Article.find((e, docs) => e ? console.log(e) : res.json(docs)))
 
-    // DELETE an article
+    // DELETE an article from the db
+    // this is working!
     app.delete('/articles/:_id', (req, res) => Article.remove({ _id: req.params._id }, e => e ? console.log(e) : res.sendStatus(200)))
 
 }
